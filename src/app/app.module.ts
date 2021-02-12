4af3d7926cb708165adc57783ffe50774b53504c
Kanthi Kiran <arjhn@localhost.localdomain>
2021-02-12 20:14:31 +0530

    basic commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FixedComponent } from './fixed/fixed.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FixedComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
